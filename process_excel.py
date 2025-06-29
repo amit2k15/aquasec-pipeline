ca48adc23509f1c5ab69a6fe0fdd9741983eb58e
amit2k15 <amitkumarnayak2k15@gmail.com>
2025-06-29 23:02:53 +0530

    Excel upload

--- process_excel.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("aquasec_july.xlsx")
-# Modify this as per your processing logic
-summary = df.describe()
-summary.to_csv("aquasec_summary.csv", index=False)
